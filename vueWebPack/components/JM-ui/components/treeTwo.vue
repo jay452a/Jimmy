16007e8a4f7e352f559d81ab833e82d5a2e25717
jay452a <562347211@qq.com>
2017-07-05 18:03:08 +0800

    2017-7-5 modify

--- vueWebPack/components/JM-ui/components/treeTwo.vue
+++ vueWebPack/components/JM-ui/components/treeTwo.vue
@@ -2,10 +2,19 @@
     <ul style="padding-left: 16px">
         <li class="JM-tree" v-for="data in tree">
             <div style="display: inline-block">
-                <i v-if="data.children&&data.children.length" :class="data.open?'':'rotate'"@click="toggle(data)"></i>
+                <i v-if="data.children&&data.children.length" :class="data.open?'rotate':''"@click="toggle(data)"></i>
             </div>
+            <label :for="data.id" class="checkbox" @click="toggleCheck(data)" v-bind:class="data.checked?'active':''"></label>
+            <input type="checkbox" :id="data.id" v-show="false"/>
             <span :data-id="data.id">{{data.label}}</span>
-            <jm-treeTwo :treeDataTwo="data.children" v-show="data.show" v-if="data.children&&data.children.length"></jm-treeTwo>
+            <transition name="fade">
+            <jm-treeTwo
+                    :treeDataTwo="data.children"
+                    v-show="data.show"
+                    v-if="data.children&&data.children.length"
+                    :linkChildren="linkChild"
+            ></jm-treeTwo>
+            </transition>
         </li>
     </ul>
 </template>
@@ -18,15 +27,25 @@
               isRotate:{
                   rotate:false
               },
-              tree:this.treeDataTwo
+              tree:this.treeDataTwo,
+              isActive:false
           }
       },
       props:{
          treeDataTwo:{
              type:Array
-         }
+         },
+          linkChildren:{//判断选中是否关联下级
+             type:Boolean
+         },
+          linkParents:{//是否选中时子关联上级
+              type:Boolean
+          }
       },
       computed:{
+          linkChild(){
+              return this.linkChildren
+          },
 
       },
       methods:{
@@ -34,6 +53,58 @@
               oneData.open=!oneData.open
               oneData.show=!oneData.show
               this.tree = Object.assign({}, this.tree);//open和show不是一开始就定义好的，所以需要重新创建一个对象，详解见vue数据响应式原理
+
+          },
+          toggleCheck(oneData){
+              var that=this
+              let allData=this.tree
+              function checkChild(data) {
+                  for(let i=0;i<data.length;i++){
+                      data[i].checked=oneData.checked
+                      if(data[i].children&&data[i].children.length){
+                          checkChild(data[i].children)
+                      }else{
+                          return
+                      }
+                  }
+              }
+              function checkParents(data,allNode) {
+                  let result
+                  let ok=false
+                  for(let j=0;j<allNode.length;j++){
+                        console.log(allNode[j].id,data.pid)
+                         if(allNode[j].id == data.pid){
+                             //allNode[i].checked=oneData.checked
+                             ok=true
+                             result=allNode[j]
+                         }else{
+                             if(!ok){
+                                 if(allNode[j].children&&allNode[j].children.length){
+                                     checkParents(data,allNode[j].children)
+                                 }else{
+                                     result=""
+                                 }
+                             }
+
+
+                         }
+                  }
+                  return result
+              }
+              //父关联子
+              if(!this.linkChildren){
+                  oneData.checked=!oneData.checked
+              }else{
+                  oneData.checked=!oneData.checked
+                  if(oneData.children&&oneData.children.length){
+                      checkChild(oneData.children)
+                  }
+              }
+              //子找到父
+              if(this.linkChildren){
+                  let p=checkParents(oneData,allData)
+                  console.log(p)
+              }
 
           }
       },
@@ -44,5 +115,11 @@
   }
 </script>
 <style>
+    .fade-enter-active, .fade-leave-active {
+        transition: opacity .5s
+    }
+    .fade-enter, .fade-leave-to /* .fade-leave-active in <2.1.8 */ {
+        opacity: 0
+    }
 
 </style>
--- vueWebPack/components/solutionList.vue
+++ vueWebPack/components/solutionList.vue
@@ -4,7 +4,13 @@
 		<jm-loading text="正在加载"></jm-loading>
 		<jm-tree :treeData="tree" v-for="tree in treeData" :allData="treeData" ref="tree"></jm-tree>
 		<button @click="chose">选择</button>
-		<jm-treeTwo :treeDataTwo="treeData"></jm-treeTwo>
+
+		<jm-treeTwo
+		:treeDataTwo="treeData"
+		:linkChildren="true"
+		:linkParents="true"
+		></jm-treeTwo>
+
 		<ul>
 			<li v-for="list in listData" @click="setData(list)" v-show="list.show">{{list.name}}</li>
 		</ul>
@@ -16,45 +22,59 @@
     let treeData=[{
             id:"1",
             label: '一级 1',
+		    checked:false,
             children: [{
                 id:"2",
                 label: '二级 1-1',
+                checked:false,
                 children: [{
                     id:"3",
-                    label: '三级 1-1-1'
+                    label: '三级 1-1-1',
+                    checked:false,
                 }]
             }]
         }, {id:"99",
             label: '一级 2',
+		    checked:false,
             children: [{
                 id:"4",
                 label: '二级 2-1',
+                checked:false,
                 children: [{
                     id:"5",
+                    checked:false,
                     label: '三级 2-1-1'
+
                 }]
             }, {
                 id:"6",
+                checked:false,
                 label: '二级 2-2',
                 children: [{
+                    checked:false,
                     label: '三级 2-2-1'
                 }]
             }]
         }, {
             id:"7",
             label: '一级 3',
+            checked:false,
             children: [{
                 id:"8",
                 label: '二级 3-1',
+                checked:false,
                 children: [{
                     id:"9",
+                    checked:false,
                     label: '三级 3-1-1'
                 }]
             }, {
                 id:"10",
+                checked:false,
                 label: '二级 3-2',
                 children: [{
                     id:"11",
+                    checked:false,
                     label: '三级 3-2-1'
                 }]
             }]
@@ -84,12 +104,16 @@
 		},
         created(){
             let data=this.treeData
-            function setData(data) {
+            function setData(data,id) {
                 for(let i=0;i<data.length;i++){
-					data[i].show=false
-					data[i].open=true
+					//data[i].show=false
+					//data[i].open=true
+					//data[i].checked=false
+					if(id){
+                        data[i].pid=id
+					}
 					if(data[i].children&&data[i].children.length){
-						setData(data[i].children)
+						setData(data[i].children,data[i].id)
 					}else{
 					    return
 					}
